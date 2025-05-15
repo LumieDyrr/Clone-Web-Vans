36e801998beaf9c6e846a7ad6aaa98a28f7d4ba0
Lumiere <muhammadadhaanbu@gmail.com>
2025-05-15 14:24:08 +0700

    Menambahkan footer

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const menuItems = document.querySelectorAll('.menu li');
 const previewImage = document.getElementById('previewImage');
 
+function toggleSection(section) {
+  section.classList.toggle("active");
+}
+
 menuItems.forEach(item => {
   item.addEventListener('mouseover', () => {
     const imgPath = item.getAttribute('data-img');
